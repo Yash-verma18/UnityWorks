432e2512758084fd2df46a84a22e51f42dd08708
recognizeyourself <recognizeyourself909@gmail.com>
2024-04-11 15:08:50 +0530

    code refactoring

---
+++ app/dashboard/employees/page.tsx
@@ -0,0 +1,7 @@
+export default function EmployeesPage() {
+  return (
+    <div>
+      <h2>Employee</h2>
+    </div>
+  );
+}
---
+++ app/dashboard/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function SettingsPage() {
+  return (
+    <div>
+      <h2>Setting Page</h2>
+    </div>
+  );
+}
