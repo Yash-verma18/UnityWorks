daea4357ccb676c6eeafda37a4d795702365f1dd
recognizeyourself <recognizeyourself909@gmail.com>
2024-04-11 15:55:31 +0530

    tabs and data cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,17 @@
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import EmpoyeesStats from "./components/employees-stats";
+
 export default function DashboardPage() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <Tabs defaultValue="employees">
+      <TabsList className="mb-4">
+        <TabsTrigger value="employees">Employee</TabsTrigger>
+        <TabsTrigger value="teams">Teams</TabsTrigger>
+      </TabsList>
+      <TabsContent value="employees">
+        <EmpoyeesStats />
+      </TabsContent>
+      <TabsContent value="teams">teams stats view</TabsContent>
+    </Tabs>
   );
 }
